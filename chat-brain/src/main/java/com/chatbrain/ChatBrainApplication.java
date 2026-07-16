5b3f7d401c03213173a41522bec73b6a10b27409
Rohit Munde <rohitvmunde@gmail.com>
2026-07-16 23:22:50 +0530

    chore: complete milestone 0 project setup

---
+++ chat-brain/src/main/java/com/chatbrain/ChatBrainApplication.java
@@ -0,0 +1,12 @@
+package com.chatbrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBrainApplication.class, args);
+	}
+}
