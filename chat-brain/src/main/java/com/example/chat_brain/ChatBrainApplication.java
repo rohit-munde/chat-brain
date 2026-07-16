4ea1d1799fa26a551c7eecfa9fa4b432606bbd70
Rohit Munde <rohitvmunde@gmail.com>
2026-07-16 22:51:12 +0530

    first commit

---
+++ chat-brain/src/main/java/com/example/chat_brain/ChatBrainApplication.java
@@ -0,0 +1,13 @@
+package com.example.chat_brain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBrainApplication.class, args);
+	}
+
+}
---
+++ chat-brain/src/test/java/com/example/chat_brain/ChatBrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chat_brain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
